acf61c4d65b3d6dacc60d962a160bfbded3894fd
alexgarmash <harmashalex@gmail.com>
2019-01-08 22:56:12 +0200

    Fix tests, add libs

--- lecture01/src/test/java/ru/atom/HelloWorldTest.java
+++ lecture01/src/test/java/ru/atom/HelloWorldTest.java
@@ -2,13 +2,12 @@ package ru.atom;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class HelloWorldTest {
     @Test
     public void getHelloWorld() throws Exception {
         assertEquals("Hello, World!", HelloWorld.getHelloWorld());
-        assertFalse("Some strange string".equals(HelloWorld.getHelloWorld()));
+        assertNotEquals("Some strange string", HelloWorld.getHelloWorld());
     }
 }
